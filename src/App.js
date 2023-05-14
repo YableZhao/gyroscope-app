f1fe2abcab9d05ed282361a4dc0f37ec84c9d139
YableZhao <yubol36400196@gmail.com>
2023-05-14 03:00:40 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alpha: 0,
+      beta: 0,
+      gamma: 0,
+    };
+  }
+
+  componentDidMount() {
+    if (window.DeviceOrientationEvent) {
+      window.addEventListener('deviceorientation', this.handleOrientation);
+    } else {
+      console.log("Sorry, your browser doesn't support Device Orientation");
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('deviceorientation', this.handleOrientation);
+  }
+
+  handleOrientation = (event) => {
+    this.setState({
+      alpha: event.alpha,
+      beta: event.beta,
+      gamma: event.gamma,
+    });
+  };
+
+  render() {
+    const { alpha, beta, gamma } = this.state;
+  
+    const dotStyle = {
+      position: 'relative',
+      top: `${50 + gamma}%`,
+      left: `${50 + beta}%`,
+      width: '10px',
+      height: '10px',
+      backgroundColor: 'red',
+      borderRadius: '50%',
+    };
+  
+    return (
+      <div>
+        <h1>Gyroscope Data:</h1>
+        <p>Alpha: {alpha}</p>
+        <p>Beta: {beta}</p>
+        <p>Gamma: {gamma}</p>
+        <div style={{position: 'relative', width: '100%', height: '400px', border: '1px solid black'}}>
+          <div style={dotStyle}></div>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
